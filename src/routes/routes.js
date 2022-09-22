17ebcb2908c888c64e2bd155e3a8811ebc9565dd
gpbezerra <gabrielpbezerra8@gmail.com>
2022-09-22 18:05:59 -0300

    mexendo routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ import { createClient } from "../controllers/clientController.js";
 import { createOperator } from "../controllers/operatorController.js";
 import { createDeliveryCompany } from "../controllers/deliveryCompanyController.js";
 import { getMenu, getImgMenu, getItems } from "../resources/mock/getMenu.js";
-import { createCartSession, addToCart, getSession } from "../controllers/cart/cartController.js";
+// import { createCartSession, addToCart, getSession } from "../controllers/cart/cartController.js";
 
 import { getSession, createCart, updateCart } from "../resources/session/session.js";
 
@@ -14,21 +14,12 @@ router.get("/menu/items", getMenu);
 router.get("/items", getItems);
 router.get("/images/:imageName", getImgMenu);
 
-// cookie
-
-router.get("/session", getSession);
-
-// Cart
-router.get("/", createCartSession);
-router.post("/adicionarItem", addToCart);
-
-router.get("/", (req, res) => {
-	res.send("início");
-});
 
 // Acessáveis por qualquer usuário (não necessita de autenticação)
+
 router.post("/cliente/cadastrar", createClient);
 
+// Sessão 
 router.get("/sessao", getSession);
 router.get("/sessao/criar-carrinho", createCart);
 router.get("/sessao/att-carrinho", updateCart);
