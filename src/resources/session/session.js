82503b1e31c0dffe04bb4f8d8f3f702778bceb30
Isabelle Rizzo <isabelle965rizzo@gmail.com>
2022-09-22 17:52:36 -0300

    teste sessão

---
+++ src/resources/session/session.js
@@ -0,0 +1,31 @@
+export function getSession(req, res) {
+	const userSession = req.session;
+	res.send({ userSession, session: req.session });
+}
+
+export function createCart(req, res) {
+	const userSession = req.session;
+	const cart = { items: [{ item: { id: "adygduyag", value: 10 }, quantity: 1 }], itemsPrice: 10 };
+	userSession.cart = cart;
+
+	res.status(201).send("ok");
+}
+
+export function updateCart(req, res) {
+	const userSession = req.session;
+	console.log(`carrinho atual: ${JSON.stringify(userSession?.cart)}`);
+
+	const cart = req.session.cart ? req.session.cart : { items: [], itemsPrice: 0 };
+
+	cart.items.push({ item: { id: "dygauydau", value: 5 }, quantity: 2 });
+
+	cart.itemsPrice = 1000;
+
+	userSession.cart = cart;
+
+	console.log(`carrinho depois: ${JSON.stringify(userSession?.cart)}`);
+
+	res.status(201).send("ok");
+}
+
+export const a = 1;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,20 @@ import { createClient } from "../controllers/clientController.js";
 import { createOperator } from "../controllers/operatorController.js";
 import { createDeliveryCompany } from "../controllers/deliveryCompanyController.js";
 
+import { getSession, createCart, updateCart } from "../resources/session/session.js";
+
 const router = Router();
+
+router.get("/", (req, res) => {
+	res.send("início");
+});
 
 // Acessáveis por qualquer usuário (não necessita de autenticação)
 router.post("/cliente/cadastrar", createClient);
+
+router.get("/sessao", getSession);
+router.get("/sessao/criar-carrinho", createCart);
+router.get("/sessao/att-carrinho", updateCart);
 
 // Acessáveis apenas por certos usuários
 // TODO: colocar camada de autenticação
